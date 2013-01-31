564fe6abc877bd39a9701b1330b8f4d2546411bc
dreag0nic <spenser.m.bray@gmail.com>
2013-01-31 11:29:06 -0600

    Added preliminary framework for the Level class.

--- PantheonPrototype/PantheonPrototype/Level.cs
+++ PantheonPrototype/PantheonPrototype/Level.cs
@@ -12,19 +12,49 @@ using Microsoft.Xna.Framework.Media;
 
 namespace PantheonPrototype
 {
-    //<summary>
-    //This is the preliminary level class.
-    //The level class handles rendering the map
-    //  and handling all entities that happen to
-    //  populate said map. These entities will be used
-    //  to build the world and create event triggering,
-    //  enemies, NPCs, etc. 
-    //
-    //This should be where the majority of the game logic
-    //  goes, via the interaction between entities.
-    //</summary>
-    
+    /// <summary>
+    /// This is the preliminary level class.
+    /// The level class handles rendering the map
+    /// and handling all entities that happen to
+    /// populate said map. These entities will be used
+    /// to build the world and create event triggering,
+    /// enemies, NPCs, etc. 
+    ///
+    /// This should be where the majority of the game logic
+    /// goes, via the interaction between entities.
+    /// 
+    /// Level content will be loaded dynamically through
+    /// some sort of WAD-type file.
+    ///</summary>
     class Level
     {
+        // Member Variable Declaration
+
+        // Object Function Declaration
+        /// <summary>
+        /// The constructor for the Level class. Basically doesn't do anything
+        /// important at this point.
+        /// </summary>
+        public Level() { }
+
+        /// <summary>
+        /// The Update function will run through the level and perform any
+        /// necessary operations for processing the frame. This includes
+        /// going through the list of active entities in the level and
+        /// updating them as well.
+        /// </summary>
+        public void Update()
+        {
+        }
+
+        /// <summary>
+        /// The Draw function will draw the level itself, as well as any
+        /// subentities that are a part of the level. Each entity will
+        /// be in charge of drawing itself, and the level will merely
+        /// draw the physical level. (Tiles, Sprites, Etc)
+        /// </summary>
+        public void Draw()
+        {
+        }
     }
 }
