9410247d0e7310ec82ab37bdc2ac23c54dd1c9f8
Bacon41 <bacon@dorm41.org>
2013-02-14 13:28:47 -0600

    Fixed the merge error...?
    Also, the laser jumping error apparently. I don't remember doing this
    though...

--- PantheonPrototype/PantheonPrototype/Level.cs
+++ PantheonPrototype/PantheonPrototype/Level.cs
@@ -89,6 +89,9 @@ namespace PantheonPrototype
             this.entities.Add("character", new PlayerCharacter(gameReference));
             this.entities["character"].Load(gameReference.Content);
 
+            //this.entities.Add("theOldMan", new OldManNPC(Vector2.Zero));
+            //this.entities["theOldMan"].Load(gameReference.Content);
+
             // This spawns the character in the right place in the map.
             foreach (MapObject obj in levelMap.ObjectLayers["Spawn"].MapObjects)
             {
