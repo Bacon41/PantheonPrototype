339722f66eda526d0c1767826790eca0232cd8eb
dreac0nic <spenser.m.bray@gmail.com>
2013-02-28 02:02:38 -0600

    Added the basics of the DialogueManager, for the sake of merge conflicts. Hopefully we can avoid them and the meat will be added later. Sl...se.eeee..p...now....

---
+++ PantheonPrototype/PantheonPrototype/DialogueManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PantheonPrototype
+{
+    /// <summary>
+    /// This class will handle the basics of dialogue and what all that entails.
+    /// This includes drawing the appropriate bubbles and text, as well as possibly
+    /// handling some basic dialogue pathing and such.
+    /// </summary>
+    class DialogueManager
+    {
+        DialogueNode currentNode;
+
+        public DialogueManager()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw()
+        {
+        }
+    }
+}
