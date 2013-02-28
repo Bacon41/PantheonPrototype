61e93a96ba8aca343719fec5d148d89e4aab6dc8
dreac0nic <spenser.m.bray@gmail.com>
2013-02-28 10:36:58 -0600

    Fleshed out the structure of the DialogueManager.

--- PantheonPrototype/PantheonPrototype/DialogueManager.cs
+++ PantheonPrototype/PantheonPrototype/DialogueManager.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace PantheonPrototype
 {
@@ -12,17 +19,57 @@ namespace PantheonPrototype
     /// </summary>
     class DialogueManager
     {
-        DialogueNode currentNode;
+        int currentTime = 0;
+        DialogueNode currentNode; // NO
 
+        /// <summary>
+        /// Constructs the basics of the DialogueManager class and prepares it to handle
+        /// dialogue and conversation.
+        /// </summary>
         public DialogueManager()
         {
         }
 
-        public void Update()
+        /// <summary>
+        /// Keeps DialogueManager up-to-date. Makes sure no conversations are still on going.
+        /// Manages the checking of exit conditions for text bubbles.
+        /// </summary>
+        public void Update(GameTime gameTime)
+        {
+            this.currentTime = gameTime;
+        }
+
+        /// <summary>
+        /// Draws all the active text bubbles in their appropriate place.
+        /// </summary>
+        public void Draw()
         {
         }
 
-        public void Draw()
+        /// <summary>
+        /// Starts a conversation with a given entity. This entity ID is used to identify which conversation to execute.
+        /// </summary>
+        /// <param name="entityName">The entity to begin conversing with. Used as a handle to pick the conversation "column."</param>
+        public void StarConversation(String entityName)
+        {
+        }
+
+        /// <summary>
+        /// Ends all current conversations and resets the conversation handles and anchors.
+        /// </summary>
+        public void EndConversation()
+        {
+        }
+
+        /// <summary>
+        /// Creates a custom text bubble with a duration with the specified parameteres.
+        /// Is not necessary for creating conversation and will be invoked automatically
+        /// via the DialogueManager.
+        /// </summary>
+        /// <param name="position">The location of the speaking point of the text bubble.</param>
+        /// <param name="text">The text the text bubble should say.</param>
+        /// <param name="duration">How long (in game time) the text bubble should last.</param>
+        public void CreateTextBubble(Vector2 position, String text, int duration)
         {
         }
     }
