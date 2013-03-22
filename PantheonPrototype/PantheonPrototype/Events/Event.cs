7ff92f9ae1d28471b10bfe2e62583d868cbc71dc
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2013-03-22 18:47:51 -0500

    Adding a description for the event class.

--- PantheonPrototype/PantheonPrototype/Events/Event.cs
+++ PantheonPrototype/PantheonPrototype/Events/Event.cs
@@ -12,6 +12,15 @@ using Microsoft.Xna.Framework.Media;
 
 namespace PantheonPrototype
 {
+    /// <summary>
+    /// A basic event.
+    /// 
+    /// Consists of a type and a list of strings. The type allows the event manager to know where
+    /// to send the event. The list of strings gives more specific information about the event.
+    /// 
+    /// Essentially, the type is an identifier of what type of event this is, and thus an
+    /// identification of the receivers. The list of strings is essentially a payload for the event.
+    /// </summary>
     class Event
     {
     }
