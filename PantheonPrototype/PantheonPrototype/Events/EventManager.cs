6fa56af8812f8aa83c399ccfb403cd799963f2c8
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2013-03-23 05:16:35 -0500

    Adding comments to the EventManager Class.

--- PantheonPrototype/PantheonPrototype/Events/EventManager.cs
+++ PantheonPrototype/PantheonPrototype/Events/EventManager.cs
@@ -12,6 +12,13 @@ using Microsoft.Xna.Framework.Media;
 
 namespace PantheonPrototype
 {
+    /// <summary>
+    /// Distributes events to the correct locations in Pantheon.
+    /// 
+    /// Classes should register functions with the EventManager to handle specific types of events.
+    /// When an event occurs, the type is identified, and all registered functions are called. They
+    /// are passed the event, whose payload gives any other relevant information to the recipient.
+    /// </summary>
     class EventManager
     {
     }
