5dd1fff3c96f0f9cea94c8b6651d199fb1f3247c
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2013-04-02 10:47:53 -0500

    Adding the triggered Objective.
    Also creating an objectives folder for the other types of objectives we
    will eventually need.

---
+++ PantheonPrototype/PantheonPrototype/Quests/Objectives/TriggerObjective.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PantheonPrototype
+{
+    /// <summary>
+    /// An objective tailored for tracking a trigger. Once activated, the objective
+    /// waits until the player has entered the target trigger. The objective is then
+    /// complete.
+    /// </summary>
+    class TriggerObjective : Objective
+    {
+    }
+}
