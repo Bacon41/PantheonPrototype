844ea46e1bca20ce4d2a6c378897667a6cb3a67a
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2013-04-20 21:19:28 -0500

    Adding skeleton structure to the LevelLoader class.

--- LevelLoad/LevelLoader.cs
+++ LevelLoad/LevelLoader.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using FuncWorks.XNA.XTiled;
 
 namespace LevelLoad
 {
@@ -22,5 +23,74 @@ namespace LevelLoad
     /// </summary>
     public class LevelLoader
     {
+        /// BEGIN: XML Parsing Section
+
+        /// <summary>
+        /// Path to the TMX map for this level.
+        /// </summary>
+        public string MapPath;
+
+        /// <summary>
+        /// Contains information to construct the player entity.
+        /// </summary>
+        public PlayerLoader Player;
+
+        /// <summary>
+        /// Contains information to construct all the enemy entities for this level.
+        /// 
+        /// Of the form <key, value> = <string, EnemyLoader>
+        /// </summary>
+        public Dictionary<string, EnemyLoader> Enemies;
+
+        /// <summary>
+        /// Contains information to construct all the NPC entities for this level.
+        /// 
+        /// Of the form <key, value> = <string, NPCLoader>
+        /// </summary>
+        public Dictionary<string, NPCLoader> NPCs;
+
+        /// <summary>
+        /// Contains information to construct all dialogue
+        /// </summary>
+        public DialogueLoader Dialogue;
+
+        /// <summary>
+        /// Contains information to construct all quests.
+        /// </summary>
+        public QuestLoader Quests;
+
+        /// <summary>
+        /// Path to the next level XML.
+        /// </summary>
+        public string NextLevelPath;
+
+        /// END: XML Parsing Section
+
+        /// <summary>
+        /// The map member into which the map located at MapPath should be loaded.
+        /// </summary>
+        private Map levelMap;
+
+        /// <summary>
+        /// Loads all dependent content in the Level.
+        /// 
+        /// Notably the levelMap.
+        /// </summary>
+        public void Load(ContentManager contentManager)
+        {
+            levelMap = contentManager.Load<Map>(MapPath);
+        }
+
+        /// <summary>
+        /// Gets the map member loaded from the MapPath member.
+        /// 
+        /// Note: the load function should be called before this function. Otherwise the map will
+        /// not be inititialized.
+        /// </summary>
+        /// <returns>A map object.</returns>
+        public Map getLevelMap()
+        {
+            return levelMap;
+        }
     }
 }
