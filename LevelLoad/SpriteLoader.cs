e21ef14b3ee9a6c38dd0f20e93cbd9a5adb5debb
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2013-04-20 22:44:17 -0500

    Beginning work on the SpriteLoader.

---
+++ LevelLoad/SpriteLoader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace LevelLoad
+{
+    /// <summary>
+    /// Loads a sprite from an XML file.
+    /// </summary>
+    class SpriteLoader
+    {
+        /// BEGIN: XML Parsing Section
+        
+        /// <summary>
+        /// Path to the sprite image.
+        /// </summary>
+        public string ImagePath;
+
+        /// <summary>
+        /// The number of rows in the image.
+        /// </summary>
+        public int Rows;
+
+        /// <summary>
+        /// The number of columns in the image.
+        /// </summary>
+        public int Columns;
+
+        /// END: XML Parsing Section
+    }
+}
